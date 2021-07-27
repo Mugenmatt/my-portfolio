b8a1286d4d7bae5540be9d86775da7407a08bd9e
Matias Godoy <matigodoy93@hotmail.com>
2021-07-26 22:00:17 -0300

    About Component

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,11 +1,94 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {
+    AboutPage,
+    ContainerChild,
+    AboutDescription,
+    Elipse,
+    MyAcronym
+} from './style'
+import PageTitle from '../../components/pageTitle/PageTitle';
+import ListTechSkills from '../../components/listTechSkills/ListTechSkills';
+import { useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
-const about = () => {
+const About = () => {
+
+    const {ref, inView} = useInView();
+    const aboutAnimation = useAnimation();
+    const elipseAnimation = useAnimation();
+    const descriptionAnimation = useAnimation();
+    const textElipseAnimation = useAnimation();
+
+    useEffect(() => {
+        console.log(inView, 'ABOUT');
+
+        // FULL ABOUT PAGE
+        if(inView) {
+            aboutAnimation.start({
+                y: 0,
+                transition: {
+                    type: 'spring', duration: 5, bounce: 0.3, when: 'beforeChildren'
+                }
+            })
+                
+            // DESCRIPTION
+            descriptionAnimation.start({
+                y: [100, 1],
+                opacity: [0, 1],
+                transition: {duration: 2, delay: 1}
+            })
+
+            // ELIPSE
+            elipseAnimation.start({
+                opacity: [0, 1],
+                scale: [1.8, 1],
+                color: ['#fff', '#A3ADCC'],
+                boxShadow: '-19px 3px 6px rgba(0, 0, 0, 0.25)',
+                transition: {delay: 0.5, duration: 1}
+            })
+
+            // TEXT ELIPSE
+            textElipseAnimation.start({
+                color: ['#fff', '#A3ADCC'],
+                transition: {delay: 0.5, duration: 3}
+            })
+
+            } else {
+                aboutAnimation.start({
+                    y: '-100vh',
+                })
+            }
+
+
+    }, [inView])
+
     return (
-        <div>
+        <AboutPage id='about' ref={ref} animate={aboutAnimation}>
             
-        </div>
+            <ContainerChild>
+
+                <PageTitle 
+                    number='01.' 
+                    text='About Me' 
+                    lineWidth='60%' 
+                />
+
+                <AboutDescription
+                    animate={descriptionAnimation}
+                >
+                    Im an argentinian front-end dev who likes to code things from scratch, and enjoy bringing ideas to life in the browser.
+                </AboutDescription>
+
+                <ListTechSkills />
+
+            </ContainerChild>
+
+            <Elipse animate={elipseAnimation} >
+                <MyAcronym animate={textElipseAnimation}>MG</MyAcronym>
+            </Elipse>
+
+        </AboutPage>
     );
 };
 
-export default about;
+export default About;
---
+++ src/pages/about/style.js
@@ -0,0 +1,79 @@
+import styled from "styled-components";
+import {motion} from 'framer-motion';
+
+export const AboutPage = styled(motion.div)`
+    background-color: #0A192F;
+    height: 100vh;
+    width: 50vw;
+    min-width: 50vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+
+    // IPAD
+    @media (max-width: 768px) and (max-height: 1024px) {
+        width: 110vw;
+    }
+
+`;
+
+export const ContainerChild = styled(motion.div)`
+    background-color: #112240;
+    border-radius: 33px;
+    padding: 5%;
+    width: 80%;
+    height: 90%;
+    
+    // SUFRACE DUO    
+    @media (max-width: 540px) and (max-height: 720px) {
+        height: 80vh;
+    }
+`;
+
+export const AboutDescription = styled(motion.p)`
+    color: #A3ADCC;
+    font-size: 1.5em;
+    margin: 0 2%;
+    @media (min-width: 2560px) and (min-height: 1440px) {
+        font-size: 2.5em;
+    }
+    // SUFACE DUO
+    @media (max-width: 540px) and (max-height: 720px) {
+        font-size: 1em;
+        margin: 5% 2%;
+    }
+`;
+
+export const Elipse = styled(motion.div)`
+    position: absolute;
+    background-color: #0A192F;
+    z-index: 100;
+    border-radius: 50%;
+    margin: auto;
+    width: 250px;
+    height: 200px;
+    right: -4%;
+    box-shadow: -19px 3px 6px rgba(0, 0, 0, 0.25);
+    // SUFACE DUO
+    @media (max-width: 540px) and (max-height: 720px) {
+        width: 200px;
+        height: 150px;
+        right: -7%;
+        box-shadow: -10px 3px 6px rgba(0, 0, 0, 0.25);
+    }
+`;
+
+export const MyAcronym = styled(motion.p)`
+    position: absolute;
+    font-size: 5em;
+    color: #A3ADCC;
+    font-weight: 700;
+    top: 50%;
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+    // SUFACE DUO
+    @media (max-width: 540px) and (max-height: 720px) {
+        font-size: 3em;
+    }
+`;
