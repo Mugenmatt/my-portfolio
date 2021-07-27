6ac972217a66534d24c0ae14aa7fff58cb226a9b
Matias Godoy <matigodoy93@hotmail.com>
2021-07-26 22:03:58 -0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import GlobalStyles from './assets/css/globalStyles';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Fragment>
+      <GlobalStyles />
+      <App />
+    </Fragment>,
   document.getElementById('root')
 );
