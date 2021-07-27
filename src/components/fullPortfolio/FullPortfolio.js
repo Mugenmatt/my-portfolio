244330ef5262f1fc6771310bb76e0e029ffe5a05
Matias Godoy <matigodoy93@hotmail.com>
2021-07-26 23:40:16 -0300

    vercel bug

--- src/components/fullPortfolio/FullPortfolio.js
+++ src/components/fullPortfolio/FullPortfolio.js
@@ -6,13 +6,13 @@ import {
 } from './style'
 import FixedLinks from '../fixedLinks/FixedLinks';
 import About from '../../pages/about/About';
-import SeparatorImg from '../separatorImg/SeparatorImg';
 import Education from '../../pages/education/Education';
 import Expirience from '../../pages/experience/Experience';
 import Work from '../../pages/work/Work';
 import Contact from '../../pages/contact/Contact';
 import HobbyTextSeparator from '../hobbyTextSeparator/HobbyTextSeparator';
 import guitarra from '../../assets/img/me/guitarra.jpg'
+import SeparatorImg from '../separatorImage/SeparatorImg';
 
 const FullPortfolio = () => {
 
@@ -29,7 +29,7 @@ const FullPortfolio = () => {
 
                 <Education />
 
-                <SeparatorImg img={guitarra} />
+                <SeparatorImg />
 
                 <Expirience />
 
